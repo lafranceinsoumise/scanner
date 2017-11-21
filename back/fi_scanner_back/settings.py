973822cd02eeb20419590a045243e2bf54d34517
Guillaume Royer <perso@guilro.com>
2017-11-21 17:20:55 +0100

    change default language

--- back/fi_scanner_back/settings.py
+++ back/fi_scanner_back/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-fr'
 
 TIME_ZONE = 'UTC'
 
