dbfe294db54d4b5eb87e1c1249c540d43e38987d
Jill Royer <perso@guilro.com>
2018-12-05 17:32:49 +0100

    ajoute le filtrage par evenement et uuid

--- back/registrations/viewsets.py
+++ back/registrations/viewsets.py
@@ -1,14 +1,12 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, mixins
 
 from registrations.models import Registration
 from registrations.serializers import RegistrationSerializer
 
 
-class RegistrationViewSet(
-    mixins.CreateModelMixin,
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    viewsets.GenericViewSet,
-):
+class RegistrationViewSet(viewsets.ModelViewSet):
     queryset = Registration.objects.all()
     serializer_class = RegistrationSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ("event", "uuid")
