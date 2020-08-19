4009a8656d5a5eebf82502e73ffadcba35e8a61e
Jill Royer <hello@jillroyer.me>
2020-08-19 15:27:19 +0200

    fix: do not authenticate on event

--- registrations/viewsets.py
+++ registrations/viewsets.py
@@ -14,6 +14,7 @@ class RegistrationViewSet(viewsets.ModelViewSet):
 
 
 class EventViewSet(viewsets.ReadOnlyModelViewSet):
+    authentication_classes = []
     permission_classes = [AllowAny]
 
     queryset = TicketEvent.objects.all()
