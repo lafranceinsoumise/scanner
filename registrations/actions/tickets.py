63dbf379c4eb244d8206cd7b17ee92632e4f5dbc
Benjamin Lemaire <benjamin.lemaire@franceinsoumise.org>
2025-08-14 20:42:41 +0200

    add ics field for invitation

--- registrations/actions/tickets.py
+++ registrations/actions/tickets.py
@@ -84,6 +84,7 @@ def gen_event_ics(registration):
     cal = Calendar()
     cal.add('prodid', '-//LaFranceinsoumise//FR')
     cal.add('version', '2.0')
+    cal.add('method', 'REQUEST')
 
     ics_event = Event()
     ics_event.add('uid', f"{registration.numero}")
